eba26c4174715fa72f0a20e9ca7972b3e17c43f8
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-11 11:56:03 -0400

    inquirer, prompt, questions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 function readmeOutput (data) {
-    return '
+    return `
     $(data.title)
     
     $(data.description)
@@ -19,5 +19,53 @@ function readmeOutput (data) {
     
     $(data.tests)
     
-    $(data.questions)'
+    $(data.questions)`
 }
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'contentTable',
+        message: 'Table of Contents',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please list installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please list usage instructions.',
+    },
+    {
+        type: 'checkboxinput',
+        name: 'license',
+        message: 'Please select a license.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How could other developers contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How may users test your application?',
+    },
+    {
+        type: 'input',
+        name: 'question',
+        message: 'Contact Information:',
+    },
+])
